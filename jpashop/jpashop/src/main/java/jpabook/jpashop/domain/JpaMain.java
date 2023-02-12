396db0e1009049ea3d05582a048b9c65e03837da
home <ytw9699>
2023-02-12 14:16:27 +0900

    feat : 테이블 중심 설계의 문제점이 있는 프로젝트 생성

---
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/domain/JpaMain.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+                          tx.begin(); //jpa 모든 데이터 변경은 트랜잭션 안에서 실행
+        try{
+            Order order = em.find(Order.class, 1L);
+
+            Long memberId = order.getMemberId();
+
+            Member member = em.find(Member.class, memberId);
+
+            System.out.println(member); //테이블 중심 설계의 문제점..
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();//닫아줘야 데이터베이스 커넥션 반환
+        }
+        emf.close();//리소스가 릴리즈 됨
+    }
+}
