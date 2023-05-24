8effbd9788c866523b4022ea7277e5fcbce568d4
home <ytw9699>
2023-05-25 08:00:02 +0900

    feat : 김영한 강의 진행 변경내용

---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/HelloController.java
@@ -0,0 +1,17 @@
+package hello.hellospring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+
+        model.addAttribute("data", "hello!!");
+
+        return "hello";//viewResover가 화면을 찾는다.
+    }
+}
+
