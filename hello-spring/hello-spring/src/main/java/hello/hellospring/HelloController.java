d4e6c87a1268b2c4b8dde425c407aa137dfb2e1a
home <ytw9699>
2023-05-28 21:02:25 +0900

    feat : MVC와 템플릿 엔진

--- hello-spring/hello-spring/src/main/java/hello/hellospring/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,6 +13,14 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
 
         return "hello";//viewResover가 화면을 찾는다.
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
+
+        model.addAttribute("name", name);
+
+        return "hello-mvc";
     }
 }
 
