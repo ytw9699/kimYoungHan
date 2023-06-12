a5706bdb1428b58f029839ac758e37468141c5fa
home <ytw9699>
2023-06-13 00:04:02 +0900

    feat : 관심사의 분리

--- core/core/src/main/java/hello/core/MemberApp.java
+++ core/core/src/main/java/hello/core/MemberApp.java
@@ -1,14 +1,16 @@
 package hello.core;
 
+import hello.core.config.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
 
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+
         //ctrl + alt + V : 생성자 자동완성
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
