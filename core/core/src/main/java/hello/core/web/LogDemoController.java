b7e01b87689d9ecdfce024629a10e22974561b16
home <ytw9699>
2023-08-15 22:18:47 +0900

    feat : 웹스코프, request 스코프 예제 만들기, 스코프와 Provider, 스코프와 프록시

---
+++ core/core/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,31 @@
+package hello.core.web;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+
+        Thread.sleep(1000);
+
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+}
