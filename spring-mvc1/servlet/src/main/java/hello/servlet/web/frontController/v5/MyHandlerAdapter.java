78fe583eacc76aa697171f085fe8b036d05a73c3
home <ytw9699>
2023-11-18 14:41:43 +0900

    feat : 핸들러 매핑과 핸들러 어댑터

--- spring-mvc1/servlet/src/main/java/hello/servlet/web/frontController/v5/MyHandlerAdapter.java
+++ spring-mvc1/servlet/src/main/java/hello/servlet/web/frontController/v5/MyHandlerAdapter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public interface MyHandlerAdapter {
-    
+
     boolean supports(Object handler);
 
     ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
