09ca2c2160f37f72502d96fa002e42771e435a94
TaeWon <ytw9699@naver.com>
2023-12-03 01:41:33 +0900

    feat : 로그인 처리하기 - 직접 만든 세션 적용

--- spring-mvc2/login/src/main/java/hello/login/web/HomeController.java
+++ spring-mvc2/login/src/main/java/hello/login/web/HomeController.java
@@ -2,12 +2,14 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,14 +17,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
    /* @GetMapping("/")
     public String home() {
         return "home";
     }*/
 
-    @GetMapping("/")
-    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId,
+    @GetMapping("/homeLoginV1")
+    public String homeLoginV1(@CookieValue(name = "memberId", required = false) Long memberId,
                             Model model) {
 
         if (memberId == null) {
@@ -37,6 +40,21 @@ public class HomeController {
         }
 
         model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+
+        //세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
+
+        //로그인
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
         return "loginHome";
     }
 }
