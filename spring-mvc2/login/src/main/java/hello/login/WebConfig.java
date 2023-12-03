37e63ba5f249edb44ce9569bfe1406554a3880fe
TaeWon <ytw9699@naver.com>
2023-12-03 18:51:33 +0900

    feat : 스프링 인터셉터 - 요청 로그

--- spring-mvc2/login/src/main/java/hello/login/WebConfig.java
+++ spring-mvc2/login/src/main/java/hello/login/WebConfig.java
@@ -2,9 +2,11 @@ package hello.login;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.servlet.Filter;
 
@@ -28,5 +30,13 @@ public class WebConfig implements WebMvcConfigurer {
                                        filterRegistrationBean.setOrder(2);
                                        filterRegistrationBean.addUrlPatterns("/*");//여기서는 안거른다
         return filterRegistrationBean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {//인터셉터는 이렇게 등록해야함
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")//하위 전부다
+                .excludePathPatterns("/css/**", "/*.ico", "/error");//이 경로만 제외
     }
 }
---
+++ spring-mvc2/login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
@@ -0,0 +1,50 @@
+package hello.login.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    public static final String LOG_ID = "logId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        request.setAttribute(LOG_ID, uuid);
+
+        if (handler instanceof HandlerMethod) { //@RequestMapping: HandlerMethod
+            HandlerMethod hm = (HandlerMethod) handler;//HandlerMethod는 호출할 컨트롤러 메서드의 모든 정보가 포함되어 있다.
+            //위와 다르게 정적 리소스의 경우는: ResourceHttpRequestHandler 다. 필요하면 분기처리 
+        }
+
+        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+        return true;//false 가 아닌 true 주면 핸들러 handler가 그대로 호출됨
+    }
+
+    @Override//예외가 발생하면 postHandle안됨
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+                                                       Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle [{}]", modelAndView);//modelAndView 반환해줌
+    }
+
+    @Override//완전히 끝날때 호출. 예외가 발생해도 호출됨
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+                                                            Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        String logId = (String) request.getAttribute(LOG_ID);
+        log.info("RESPONSE [{}][{}][{}]", logId, requestURI, handler);
+
+        if (ex != null) {//에러 찍기
+            log.error("afterCompletion error!!", ex);
+        }
+    }
+}
