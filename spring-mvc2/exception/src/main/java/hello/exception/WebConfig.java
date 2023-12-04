3859cae92f2dad304ae9d829c597692276a40c9b
TaeWon <ytw9699@naver.com>
2023-12-04 19:50:53 +0900

    feat : 서블릿 예외 처리 - 필터

---
+++ spring-mvc2/exception/src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,23 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+
+@Configuration//빈으로 등록
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);//2가지 경우 모두 호출
+        return filterRegistrationBean;
+    }
+}
