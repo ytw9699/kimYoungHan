a011b60234a61f046e2614219810e8c3bb89fd54
TaeWon <ytw9699@naver.com>
2023-12-04 23:46:54 +0900

    feat : 시작 - api 예외처리

--- spring-mvc2/exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ spring-mvc2/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,10 +6,9 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     //서블릿 컨테이너가 제공해준걸 사용하자. web.xml대신 사용하는것. 웹서버(톰캣)를 커스터마이징하자
-
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
         //스프링부트까 뜰때 톰캣에 아래를 등록해둠. 그래서 톰캣까지 예외발생해서오면 다시 해당 컨트롤러 호출
