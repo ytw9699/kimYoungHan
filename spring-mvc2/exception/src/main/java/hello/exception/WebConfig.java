7fac0ea3959e425698a9341bb81e3b8497c97096
TaeWon <ytw9699@naver.com>
2023-12-04 20:16:27 +0900

    feat : 서블릿 예외 처리 - 인터셉터

--- spring-mvc2/exception/src/main/java/hello/exception/WebConfig.java
+++ spring-mvc2/exception/src/main/java/hello/exception/WebConfig.java
@@ -1,9 +1,11 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
@@ -11,13 +13,22 @@ import javax.servlet.Filter;
 @Configuration//빈으로 등록
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Bean//중복 원하지않으면 제거
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1);
         filterRegistrationBean.addUrlPatterns("/*");
-        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);//2가지 경우 모두 호출
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        //2가지 경우 모두 호출 원하지 않으면 하나만 
         return filterRegistrationBean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");//오류 페이지 경로제외
     }
 }
