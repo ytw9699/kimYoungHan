f3523082438f83f56363d85bfd1a57acb3ab673c
TaeWon <ytw9699@naver.com>
2023-12-12 12:54:18 +0900

    fix : 필드와 컬럼 매핑 - 주석 추

--- HelloJpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ HelloJpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity//jpa가 로딩될때 jpa를 사용하는 애로인식하고 관리한다.
@@ -14,17 +16,22 @@ public class Member {
     @Column(name="phoneNum", columnDefinition = "varchar(100) default 'EMPTY'")
     private String phoneNum;
 
+    //private Integer age;
     @Column(updatable = false)//업데이트 반영안되게
     private int age;
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING)//디비에는 enum타입이 없어서 varchar로 들어가게 하자. ORDINAL는 숫자로들어감
     private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createDate;
+    @Temporal(TemporalType.TIMESTAMP)//TIMESTAMP는 날짜시간둘다 적용하는것. 왜냐면 디비는 날짜,시간,날짜시간을 구분함
+    private Date createDate;//과거버전이면 date 사용, 최신이면 LocalDateTime
     @Temporal(TemporalType.TIMESTAMP)
     private Date updateDate;
-    @Lob
+
+    private LocalDate testLocalDate;//날짜 date타입
+    private LocalDateTime testLocalDateTime;//TIMESTAMP타입 날짜 시간 둘다 적용 보통 하이버네이트 최신이면 이걸로 씀
+
+    @Lob//디비에 varchar를 넘어서는 큰 컨텐츠 CLOB, 문자가 아닌 나머지는 BLOB
     private String description;
-    @Transient
+    @Transient//디비랑 맵핑안하고싶을때
     private int temp;//메모리에만 있음 캐싱필요할때
 
     public Member(){} //jpa는 내부적으로 리플렉션 같은것을 쓰기에 동적으로 객체생성하려면 기본생성자 있어야함
