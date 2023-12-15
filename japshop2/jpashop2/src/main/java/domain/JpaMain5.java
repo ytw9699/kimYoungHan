87a2f66ccaa01c76d01b1b68fcece5857d955646
TaeWon <ytw9699@naver.com>
2023-12-15 12:55:50 +0900

    fix : 다대다 [N:M] - 코드 추가

---
+++ japshop2/jpashop2/src/main/java/domain/JpaMain5.java
@@ -0,0 +1,29 @@
+package domain;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain5 {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+                          tx.begin();
+        try{
+
+            tx.commit();
+
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
