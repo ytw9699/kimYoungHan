a6628d6d5ea21e01ccba814d2730d70495f4ca46
TaeWon <ytw9699@naver.com>
2023-12-19 12:40:03 +0900

    fix : 실전 예제 6 - 값 타입 매핑

--- jpashop/jpashop/src/main/java/jpabook/jpashop/domain/embed/Address.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/domain/embed/Address.java
@@ -1,12 +1,20 @@
 package jpabook.jpashop.domain.embed;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
+
+    @Column(length = 100)//이렇게 공통으로 관리
     private String city;
     private String street;
     private String zipcode;
+
+    public String fullAddress(){//의미있는 메소드를 만든다. 객체지향적으로
+        return getCity()+" "+getStreet()+ " "+getZipcode();
+    }
 
     public Address() {
     }
@@ -39,5 +47,20 @@ public class Address {
 
     public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) &&
+                Objects.equals(getStreet(), address.getStreet()) &&
+                Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
     }
 }
