ff955d998e8852e5ec8f1ce289b091e0232d9b26
TaeWon <ytw9699@naver.com>
2023-12-19 17:18:34 +0900

    fix : 기본 문법과 쿼리 API2

--- jpashop/jpashop/src/main/java/jpabook/jpashop/domain/JpaMain7.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/domain/JpaMain7.java
@@ -1,10 +1,8 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.jpql.Member7;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain7 {
     public static void main(String[] args) {
@@ -20,8 +18,31 @@ public class JpaMain7 {
         try{
             Member7 member = new Member7();
                     member.setUsername("member7");
-
+                    member.setAge(10);
             em.persist(member);
+
+            TypedQuery<Member7> query1 = em.createQuery("select m from Member7 as m", Member7.class);
+
+            List<Member7> resultList = query1.getResultList();
+
+            for (Member7 member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
+
+            TypedQuery<Member7> query2 = em.createQuery("select m from Member7 as m where m.id = 1L" , Member7.class);
+            Member7 singleResult = query2.getSingleResult();
+            System.out.println("singleResult = " + singleResult);
+
+            TypedQuery<String> query3 = em.createQuery("select m.username from Member7 as m", String.class);
+            System.out.println("query3 = " + query3.getSingleResult());
+            Query query4 = em.createQuery("select m.username, m.age from Member7 as m");//반환 타입 명확하지 않을때
+            System.out.println("query4 = " + query4);
+
+            Member7 singleResult1 = em.createQuery("select m from Member7 as m where m.username = :username" , Member7.class)
+                                    .setParameter("username","member7")
+                                    .getSingleResult();
+            System.out.println("singleResult1 = " + singleResult1);
+
             tx.commit();
 
         }catch (Exception e){
