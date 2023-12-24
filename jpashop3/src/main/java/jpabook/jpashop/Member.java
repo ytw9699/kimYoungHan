640802fca243902cd4516976fd43065052a8344a
TaeWon <ytw9699@naver.com>
2023-12-24 22:58:51 +0900

    feat : JPA와 DB 설정, 동작확인

---
+++ jpashop3/src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+}
