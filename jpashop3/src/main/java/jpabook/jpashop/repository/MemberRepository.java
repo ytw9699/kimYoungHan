eb31fcfe48b7ff1418506782514eda84d5905cfd
TaeWon <ytw9699@naver.com>
2023-12-27 15:07:11 +0900

    feat : 회원 리포지토리 개발

--- jpashop3/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop3/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -4,11 +4,12 @@ import jpabook.jpashop.domain.Member;
 import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
 
-    @PersistenceContext
+    @PersistenceContext//빈주입해줌
     private EntityManager em;
 
     public Long save(Member member) {
@@ -16,7 +17,26 @@ public class MemberRepository {
         return member.getId();
     }
 
+    public void save2(Member member) {
+        em.persist(member);
+    }
+
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String username) {
+        return em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", username)
+                .getResultList();
     }
 }
