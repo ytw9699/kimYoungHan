cfd60133aba5cf8dbfeac8614423ab8f5944ded1
TaeWon <ytw9699@naver.com>
2023-12-29 14:35:38 +0900

    feat : 상품 리포지토리 개발

---
+++ jpashop3/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {//저장하기전까지 id값 없음
+            em.persist(item);
+        } else {//디비에 등록된걸 다시 가져온거라고보자
+            em.merge(item);//업데이트라 일단 생각하자
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
