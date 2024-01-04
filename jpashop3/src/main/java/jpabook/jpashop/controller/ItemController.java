0fcaa154ecca10304082dc242ba196a4c96b8683
TaeWon <ytw9699@naver.com>
2024-01-05 08:19:02 +0900

    feat : 상품 수정

--- jpashop3/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop3/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
 
@@ -42,6 +44,40 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    @GetMapping("items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+
+        Book item = (Book) itemService.findOne(itemId);//심플하게 책만 일단 가져와서 수정한다. 그래서 캐스팅함
+
+        BookForm form = new BookForm();
+                 form.setId(item.getId());
+                 form.setName(item.getName());
+                 form.setPrice(item.getPrice());
+                 form.setStockQuantity(item.getStockQuantity());
+                 form.setAuthor(item.getAuthor());
+                 form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("items/{itemId}/edit")//아이템 id조작할수있기때문에, 서버단에서 이 유저가 작성한건지 확인해야함
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+
+        Book book = new Book();
+             book.setId(form.getId());
+             book.setName(form.getName());
+             book.setPrice(form.getPrice());
+             book.setStockQuantity(form.getStockQuantity());
+             book.setAuthor(form.getAuthor());
+             book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+
+        return "redirect:/items";
     }
 }
 
