fc71f2b8716b2a8d378e79862fd491b35dd012a8
TaeWon <ytw9699@naver.com>
2024-01-06 11:46:43 +0900

    feat : 간단한 주문 조회 V4: JPA에서 DTO로 바로 조회

--- jpashop3/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop3/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -4,6 +4,8 @@ import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.*;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +28,7 @@ import static java.util.stream.Collectors.toList;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository; //의존관계 주입
 
     /**
      * V1. 엔티티 직접 노출
@@ -71,6 +74,12 @@ public class OrderSimpleApiController {
                                             .collect(toList());
         return result;
     }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
+
 
     @Data
     static class SimpleOrderDto {
