8a25c2ebccc760741d46a9b5bf3f6cdb34ff020e
TaeWon <ytw9699@naver.com>
2024-01-11 11:45:17 +0900

    fix : 공통 인터페이스 분석

--- dataJpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ dataJpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -2,7 +2,10 @@ package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
+import java.util.List;
 
 //스프링 데이터 jpa 레포지토리임
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsername(String username);//구현하지 않아도 동작. 쿼리메소드 기능!
 }
