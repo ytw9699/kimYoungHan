43b37acd91882b4a34ba9243f7657af0dff5c2d2
TaeWon <ytw9699@naver.com>
2024-01-16 13:00:24 +0900

    feat : Web 확장 - 페이징과 정렬

--- dataJpa/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ dataJpa/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,10 +1,16 @@
 package study.datajpa.controller;
 
+import java.util.stream.IntStream;
 import javax.annotation.PostConstruct;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -25,8 +31,17 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> getMembers(
+            @PageableDefault(size = 5, sort = "username", direction = Sort.Direction.DESC) Pageable pageable) {
+            Page<Member> page = memberRepository.findAll(pageable);
+        return page.map(member -> new MemberDto(member));
+    }
+
     @PostConstruct
     private void init() {
-        memberRepository.save(new Member("userA"));
+        IntStream.range(1, 100).forEach(i -> {
+            memberRepository.save(new Member("user" + i, i));
+        });
     }
 }
