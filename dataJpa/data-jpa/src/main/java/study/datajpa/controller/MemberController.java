f0a6e8f05d0da8af86db6a6da1bf5ce75da36671
TaeWon <ytw9699@naver.com>
2024-01-16 11:56:41 +0900

    feat  : Web 확장 - 도메인 클래스 컨버터

---
+++ dataJpa/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,32 @@
+package study.datajpa.controller;
+
+import javax.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMembers(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    private void init() {
+        memberRepository.save(new Member("userA"));
+    }
+}
