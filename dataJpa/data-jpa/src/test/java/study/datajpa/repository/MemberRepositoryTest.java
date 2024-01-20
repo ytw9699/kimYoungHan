f4a1e4f02779229e5c462dac7c355b1929b7bbed
TaeWon <ytw9699@naver.com>
2024-01-20 11:27:01 +0900

    feat : 네이티브 쿼리-1

--- dataJpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ dataJpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -369,6 +369,25 @@ class MemberRepositoryTest {
             System.out.println("name = " + name);
         }
     }
+
+    @Test
+    public void nativeQuery(){
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        Member result = repository.findByNativeQuery("m1");
+        System.out.println("result = " + result);
+    }
 }
 
 
