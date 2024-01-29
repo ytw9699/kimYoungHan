b0ac5151ca2f7e2c6c7dcccee164fa58a14dc0cf
TaeWon <ytw9699@naver.com>
2024-01-29 22:57:38 +0900

    feat : Scanner - 반복 예제

--- Java/java-start/src/scanner/Scanner3.java
+++ Java/java-start/src/scanner/Scanner3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Scanner3 {
 
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("첫 번째 숫자를 입력하세요:");
