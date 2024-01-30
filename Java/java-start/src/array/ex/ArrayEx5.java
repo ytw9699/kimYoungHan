736b24a811d8c85dd4ffec73ca8bc0bb7a4df331
TaeWon <ytw9699@naver.com>
2024-01-30 20:04:03 +0900

    feat : 문제와 풀이2

--- Java/java-start/src/array/ex/ArrayEx5.java
+++ Java/java-start/src/array/ex/ArrayEx5.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ArrayEx5 {
 
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("입력 받을 숫자의 개수를 입력하세요:");
         int count = scanner.nextInt();
