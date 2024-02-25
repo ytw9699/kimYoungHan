4eb49a48fcafe7b1a4f0c1216e375a8d5b4983c7
TaeWon <ytw9699@naver.com>
2024-02-26 08:44:28 +0900

    feat : 데이터베이스 연결

---
+++ spring-db1/jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {//상수
+    public static final String URL = "jdbc:h2:tcp://localhost/~/jdbctest";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
