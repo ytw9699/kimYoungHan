1640f894a3339ff667dd4fe1623b02641e157afb
TaeWon <ytw9699@naver.com>
2024-02-26 09:08:51 +0900

    feat : JDBC 개발 - 등록

---
+++ spring-db1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,67 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import java.sql.*;
+
+/**
+ * JDBC - DriverManager 사용
+ */
+@Slf4j
+public class MemberRepositoryV0 {
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;//PreparedStatement이걸로 쿼리날림
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);//sql 넘김
+            pstmt.setString(1, member.getMemberId());//파라미터 바인딩
+            pstmt.setInt(2, member.getMoney());
+            //SQL Injection 공격을 예방하려면 PreparedStatement 를 통한 파라미터 바인딩 방식 사용
+            pstmt.executeUpdate();//디비에 실행
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;//로그만 남기고 던짐
+        } finally {//호출보장
+            close(con, pstmt, null);
+            //리소스 반환하지않으면 커넥션이 끊어지지않고 유지되는 문제. 결과적으로 커넥션 부족장애발생
+        }
+
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {//예외가 터져도  stmt.close();  con.close();는 실행한다
+                log.info("error", e);
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+}
