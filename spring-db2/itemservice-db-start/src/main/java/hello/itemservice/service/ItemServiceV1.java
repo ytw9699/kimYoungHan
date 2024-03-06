40e906cca5995f90222ade8cd36ae5d9e324560e
TaeWon <ytw9699@naver.com>
2024-03-07 00:03:40 +0900

    feat : 클래스 프록시 확인

--- spring-db2/itemservice-db-start/src/main/java/hello/itemservice/service/ItemServiceV1.java
+++ spring-db2/itemservice-db-start/src/main/java/hello/itemservice/service/ItemServiceV1.java
@@ -5,10 +5,12 @@ import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 
 @Service
 @RequiredArgsConstructor//사실 서비스는 인터페이스 구현 잘안함 바뀔일이 없기 때문에 하지만 여기선 v1,2로 이어지기 때문
@@ -34,6 +36,7 @@ public class ItemServiceV1 implements ItemService {//단순한 위임뿐
 
     @Override
     public List<Item> findItems(ItemSearchCond cond) {
+        log.info("itemRepositoryclass={}",itemRepository.getClass());
         return itemRepository.findAll(cond);
     }
 }
