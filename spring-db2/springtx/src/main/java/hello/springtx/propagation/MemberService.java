2a2aea4764617db1617df3abb24b2ab247393655
TaeWon <ytw9699@naver.com>
2024-03-13 10:44:18 +0900

    feat : 트랜잭션 전파 활용6 - 복구 REQUIRED

--- spring-db2/springtx/src/main/java/hello/springtx/propagation/MemberService.java
+++ spring-db2/springtx/src/main/java/hello/springtx/propagation/MemberService.java
@@ -26,4 +26,23 @@ public class MemberService {
         logRepository.save(logMessage);
         log.info("== logRepository 호출 종료 ==");
     }
+
+    @Transactional
+    public void joinV2(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        log.info("== memberRepository 호출 시작 ==");
+        memberRepository.save(member);
+        log.info("== memberRepository 호출 종료 ==");
+
+        log.info("== logRepository 호출 시작 ==");
+        try {
+            logRepository.save(logMessage);
+        } catch (RuntimeException e) {
+            log.info("log 저장에 실패했습니다. logMessage={}", logMessage.getMessage());
+            log.info("정상 흐름 반환");
+        }
+        log.info("== logRepository 호출 종료 ==");
+    }
 }
