53dad6b66aecc3ecc672e169b3ef65890e1e9576
TaeWon <ytw9699@naver.com>
2024-03-14 11:27:32 +0900

    feat : 쓰레드 로컬 동기화 - 적용

--- spring-high-class/advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ spring-high-class/advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -2,14 +2,20 @@ package hello.advanced;
 
 import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogTraceConfig {
 
-    @Bean
+  /*  @Bean
     public LogTrace logTrace() {
         return new FieldLogTrace();//싱글톤 등록
+    }*/
+
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
     }
 }
