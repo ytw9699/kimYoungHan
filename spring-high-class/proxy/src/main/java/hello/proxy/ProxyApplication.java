c3f5b3c7b83522dbac46624ff829c45a46a89e98
TaeWon <ytw9699@naver.com>
2024-03-27 01:00:50 +0900

    feat : 예제 프로젝트 만들기 v1

--- spring-high-class/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring-high-class/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,13 +1,15 @@
 package hello.proxy;
 
+import hello.proxy.app.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "hello.proxy.app")
+//주의 : hello.proxy.app하위만 컴포넌트 스캔하도록하는 설정 왜냐하면 수동으로 빈등록해서 v1~5까지 바꿀것이라서
 public class ProxyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
-
 }
