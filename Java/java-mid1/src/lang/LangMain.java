228f8060daaec59129fbde759e99e5c60ea902f3
TaeWon <ytw9699@naver.com>
2024-03-28 19:26:20 +0900

    feat : java.lang 패키지 소개

---
+++ Java/java-mid1/src/lang/LangMain.java
@@ -0,0 +1,9 @@
+package lang;
+
+//import  java.lang.System;//생략해도 임포트 됨
+
+public class LangMain {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
